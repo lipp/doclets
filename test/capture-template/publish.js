5c014d111109b9eedcb00e7b5332a54b72cb4c1a
Gerhard Preuss <gelipp@gmail.com>
2015-12-10 11:33:00 +0100

    add basic module structuring

---
+++ test/capture-template/publish.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+
+exports.publish = function(taffyDb, opts) {
+    var doclets = [];
+    taffyDb().each(function(doclet) {
+        doclets.push(doclet);
+    });
+    var data = JSON.stringify(doclets, null, '\t');
+    fs.writeFileSync('./taffy.json', data);
+};
