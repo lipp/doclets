f09d6beaf51127f8a5e6d25f9d3e52a4da04d82c
Gerhard Preuss <gelipp@gmail.com>
2015-12-13 21:30:22 +0100

    asd

--- test/publish_spec.js
+++ test/publish_spec.js
@@ -10,7 +10,15 @@ describe('The publish module', function() {
 
     describe('using the acme-jsdoc-example data', function() {
         before(function(done) {
-            var jsdoc = child_process.spawn('./node_modules/.bin/jsdoc', ['-r', './node_modules/acme-jsdoc-example/src', '-t', './test/capture-template']);
+            var jsdoc = child_process.spawn('./node_modules/.bin/jsdoc', [
+                //'-r', './node_modules/jade/lib',
+                '-r', '../node-jet/lib/',
+                //'./node_modules/angular/angular.js',
+                //'./node_modules/morgan/index.js',
+                //						'-r', './node_modules/acme-jsdoc-example/src', 
+                '-t', './test/capture-template',
+                '-c', 'jsdoc.conf'
+            ]);
             //jsdoc.stdout.pipe(process.stdout);
             jsdoc.on('close', function(code) {
                 if (code === 0) {
@@ -30,7 +38,7 @@ describe('The publish module', function() {
             before(function() {
                 html = publish.render(taffyData, {});
                 fs.writeFileSync('test.html', html);
-                console.log(html);
+                //    console.log(html);
             });
 
             it('html is string', function() {
