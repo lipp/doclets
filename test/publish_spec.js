ce3a4c95fa40368949919b9e935adabafde5ae5a
Gerhard Preuss <gelipp@gmail.com>
2015-12-16 13:10:47 +0100

    s

--- test/publish_spec.js
+++ test/publish_spec.js
@@ -5,39 +5,41 @@ var fs = require('fs');
 var _ = require('underscore');
 var recursive = require('recursive-readdir');
 
-describe('The publish module', function() {
-    var taffyData;
+describe('The publish module', function () {
+	var taffyData;
 
-    describe('using the acme-jsdoc-example data', function() {
-        before(function(done) {
+	describe('using the acme-jsdoc-example data', function () {
+		before(function (done) {
 
-            recursive('../node-jet/lib/', function(err, files) {
-                if (err) {
-                    done(err);
-                    return;
-                }
-                taffyData = publish.createTaffyData(files);
-                done();
-            });
-            //'-r', './node_modules/jade/lib',
-            //'./node_modules/angular/angular.js',
-            //'./node_modules/morgan/index.js',
-            //						'-r', './node_modules/acme-jsdoc-example/src', 
-        });
+			recursive('../node-jet/lib/', function (err, files) {
+				if (err) {
+					done(err);
+					return;
+				}
+				taffyData = publish.createTaffyData(files);
+				done();
+			});
+			//'-r', './node_modules/jade/lib',
+			//'./node_modules/angular/angular.js',
+			//'./node_modules/morgan/index.js',
+			//						'-r', './node_modules/acme-jsdoc-example/src', 
+		});
 
-        describe('html= publish.render method', function() {
-            var html;
-            before(function() {
-                html = publish.render(taffyData, {});
-                fs.writeFileSync('test.html', html);
-                //    console.log(html);
-            });
+		describe('html= publish.render method', function () {
+			var html;
+			before(function () {
+				html = publish.render(taffyData, {
+					repoBase: 'http://github.com/lipp/node-jet/blob/master/lib/jet'
+				});
+				fs.writeFileSync('test.html', html);
+				//    console.log(html);
+			});
 
-            it('html is string', function() {
-                assert.equal(typeof(html), 'string');
-            });
-        });
+			it('html is string', function () {
+				assert.equal(typeof (html), 'string');
+			});
+		});
 
 
-    });
+	});
 });
--- test/utils_spec.js
+++ test/utils_spec.js
@@ -4,110 +4,110 @@ var publish = require('../lib/publish');
 var _ = require('underscore');
 var recursive = require('recursive-readdir');
 
-describe('The utils module', function() {
-    var taffyData;
+describe('The utils module', function () {
+	var taffyData;
 
-    describe('using the acme-jsdoc-example data', function() {
-        before(function(done) {
-            recursive('./node_modules/acme-jsdoc-example/src', function(err, files) {
-                if (err) {
-                    done(err);
-                    return;
-                }
-                taffyData = publish.createTaffyData(files);
-                done();
-            });
-        });
+	describe('using the acme-jsdoc-example data', function () {
+		before(function (done) {
+			recursive('./node_modules/acme-jsdoc-example/src', function (err, files) {
+				if (err) {
+					done(err);
+					return;
+				}
+				taffyData = publish.createTaffyData(files);
+				done();
+			});
+		});
 
 
-        it('taffyData is TAFFY', function() {
-            assert.ok(taffyData.TAFFY);
-        });
+		it('taffyData is TAFFY', function () {
+			assert.ok(taffyData.TAFFY);
+		});
 
-        describe('utils.buildHierarchy', function() {
-            var modules;
-            before(function() {
-                modules = utils.buildHierarchy(taffyData);
-            });
+		describe('utils.buildHierarchy', function () {
+			var modules;
+			before(function () {
+				modules = utils.buildHierarchy(taffyData);
+			});
 
-            it('_.keys(modules) are the module longnames', function() {
-                assert.deepEqual(_.keys(modules), ['module:funny', 'module:heavy', '_GLOBAL']);
-            });
+			it('_.keys(modules) are the module longnames', function () {
+				assert.deepEqual(_.keys(modules), ['module:funny', 'module:heavy', '_GLOBAL']);
+			});
 
-            it('modules._GLOBAL.functions is correct', function() {
-                var functions = _.indexBy(modules._GLOBAL.functions, 'longname');
-                assert.equal(_.size(functions), 1);
-                assert.ok(functions.superFunc);
-            });
+			it('modules._GLOBAL.functions is correct', function () {
+				var functions = _.indexBy(modules._GLOBAL.functions, 'longname');
+				assert.equal(_.size(functions), 1);
+				assert.ok(functions.superFunc);
+			});
 
-            it('modules._GLOBAL.constants is correct', function() {
-                var constants = _.indexBy(modules._GLOBAL.constants, 'longname');
-                assert.equal(_.size(constants), 2);
-                assert.ok(constants.greet);
-                assert.ok(constants.config);
-            });
+			it('modules._GLOBAL.constants is correct', function () {
+				var constants = _.indexBy(modules._GLOBAL.constants, 'longname');
+				assert.equal(_.size(constants), 2);
+				assert.ok(constants.greet);
+				assert.ok(constants.config);
+			});
 
-            it('modules._GLOBAL.interfaces is correct', function() {
-                assert.equal(modules._GLOBAL.interfaces.length, 0);
-            });
+			it('modules._GLOBAL.interfaces is correct', function () {
+				assert.equal(modules._GLOBAL.interfaces.length, 0);
+			});
 
-            it('modules._GLOBAL.classes is correct', function() {
-                assert.equal(modules._GLOBAL.classes.length, 0);
-            });
+			it('modules._GLOBAL.classes is correct', function () {
+				assert.equal(modules._GLOBAL.classes.length, 0);
+			});
 
-            it('modules["module:heavy"].interfaces is correct', function() {
-                var interfaces = _.indexBy(modules['module:heavy'].interfaces, 'longname');
-                assert.equal(_.size(interfaces), 1);
-                assert.ok(interfaces['module:heavy~Tool']);
-            });
+			it('modules["module:heavy"].interfaces is correct', function () {
+				var interfaces = _.indexBy(modules['module:heavy'].interfaces, 'longname');
+				assert.equal(_.size(interfaces), 1);
+				assert.ok(interfaces['module:heavy~Tool']);
+			});
 
-            it('modules["module:heavy"].classes is correct', function() {
-                var classes = _.indexBy(modules['module:heavy'].classes, 'longname');
-                assert.equal(_.size(classes), 2);
-                assert.ok(classes['module:heavy~Bomb']);
-                assert.ok(classes['module:heavy~Hammer']);
-            });
+			it('modules["module:heavy"].classes is correct', function () {
+				var classes = _.indexBy(modules['module:heavy'].classes, 'longname');
+				assert.equal(_.size(classes), 2);
+				assert.ok(classes['module:heavy~Bomb']);
+				assert.ok(classes['module:heavy~Hammer']);
+			});
 
-            it('modules["module:heavy"].functions is correct', function() {
-                assert.equal(modules['module:heavy'].functions.length, 0);
-            });
+			it('modules["module:heavy"].functions is correct', function () {
+				assert.equal(modules['module:heavy'].functions.length, 0);
+			});
 
-            it('modules["module:heavy"].constants is correct', function() {
-                assert.equal(modules['module:heavy'].constants.length, 0);
-            });
+			it('modules["module:heavy"].constants is correct', function () {
+				assert.equal(modules['module:heavy'].constants.length, 0);
+			});
 
-            it('heavy/Tool has correct member', function() {
-                var interfaces = _.indexBy(modules['module:heavy'].interfaces, 'longname');
-                assert.equal(interfaces['module:heavy~Tool'].members.length, 1);
-                assert.equal(interfaces['module:heavy~Tool'].members[0].longname, 'module:heavy~Tool#action');
-            });
+			it('heavy/Tool has correct member', function () {
+				var interfaces = _.indexBy(modules['module:heavy'].interfaces, 'longname');
+				assert.equal(interfaces['module:heavy~Tool'].members.length, 1);
+				assert.equal(interfaces['module:heavy~Tool'].members[0].longname, 'module:heavy~Tool#action');
+			});
 
-            it('funny/Coyote has correct member', function() {
-                var members = modules['module:funny'].classes[0].members;
-                console.log(members[0]);
-                assert.equal(members.length, 3);
-            });
+			it('funny/Coyote has correct member', function () {
+				var members = modules['module:funny'].classes[0].members;
+				console.log(members[0]);
+				assert.equal(members.length, 3);
+			});
 
-            it('modules["module:funny"].classes is correct', function() {
-                var classes = _.indexBy(modules['module:funny'].classes, 'longname');
-                assert.equal(_.size(classes), 1);
-                assert.ok(classes['module:funny~Coyote']);
-            });
+			it('modules["module:funny"].classes is correct', function () {
+				var classes = _.indexBy(modules['module:funny'].classes, 'longname');
+				assert.equal(_.size(classes), 1);
+				assert.ok(classes['module:funny~Coyote']);
+			});
 
-            it('modules["module:funny"].functions is correct', function() {
-                assert.equal(modules['module:funny'].functions.length, 0);
-            });
+			it('modules["module:funny"].functions is correct', function () {
+				assert.equal(modules['module:funny'].functions.length, 0);
+			});
 
-            it('modules["module:funny"].constants is correct', function() {
-                assert.equal(modules['module:funny'].constants.length, 0);
-            });
+			it('modules["module:funny"].constants is correct', function () {
+				assert.equal(modules['module:funny'].constants.length, 0);
+			});
 
-            it('modules["module:funny"].interfaces is correct', function() {
-                assert.equal(modules['module:funny'].interfaces.length, 0);
-            });
+			it('modules["module:funny"].interfaces is correct', function () {
+				assert.equal(modules['module:funny'].interfaces.length, 0);
+			});
 
-        });
-    });
+		});
+	});
 
 
 
