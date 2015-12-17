aece2246008db1509baf902c509781e34ddf8d93
Gerhard Preuss <gelipp@gmail.com>
2015-12-17 11:26:45 +0100

    mo

--- beautify.sh
+++ beautify.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
 find . -name "*.js" | grep -v node_modules | xargs node_modules/.bin/js-beautify -j -r -t --good-stuff
+node_modules/.bin/js-beautify -j -r -t --good-stuff ./bin/ghostdoc
