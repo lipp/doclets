5cd7a348f2a0ec87adff2d2f795655c228cedeb6
Gerhard Preuss <gelipp@gmail.com>
2015-12-18 16:27:38 +0100

    add basic articles template

--- test/toc_spec.js
+++ test/toc_spec.js
@@ -4,6 +4,15 @@ var fs = require('fs');
 var sinon = require('sinon');
 
 describe('The toc module', function () {
+	before(function () {
+
+		toc = new toc.Toc('test.json');
+	});
+
+	after(function () {
+		toc.destroy();
+	});
+
 	it('.add(...) some stuff', function () {
 		assert.doesNotThrow(function () {
 			toc.add('http://bla.com', 'master', 1);
@@ -11,14 +20,11 @@ describe('The toc module', function () {
 			toc.add('http://bla.com', 'master', 4); // should overwrite 1
 			toc.add('http://asd.com', 'master', 3);
 		});
-		assert.doesNotThrow(function () {
-			fs.statSync('./toc.json');
-		});
 	});
 
 	it('.load(each) calls each for every entry', function () {
 		var eachSpy = sinon.spy();
-		toc.load(eachSpy);
+		toc.each(eachSpy);
 		assert(eachSpy.calledWith('http://bla.com', 'master', 4));
 		assert(eachSpy.calledWith('http://bla.com', 'v1.0.1', 2));
 		assert(eachSpy.calledWith('http://asd.com', 'master', 3));
