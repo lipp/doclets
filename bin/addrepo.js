267674142caa3fd0dfec958215fb3186d7234446
Gerhard Preuss <gelipp@gmail.com>
2015-12-22 21:13:57 +0100

    asd

--- bin/addrepo.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-var clone = require('../lib/clone');
-
-clone.autodoc(process.argv[2], process.argv[3], process.argv[4]);
--- bin/server.js
+++ bin/server.js
@@ -1,10 +1,2 @@
-var server = require('../lib/server').init();
 var db = require('../lib/db');
-db.init(function (err) {
-	if (err) {
-		console.error(err);
-		process.exit(1);
-	} else {
-		server.listen(8083);
-	}
-});
+var server = require('../lib/server').init(8080, db);
