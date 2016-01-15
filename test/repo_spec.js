783dcaff6c49ecf98eb58314ec723515bda50086
Gerhard Preuss <gelipp@gmail.com>
2016-01-15 23:06:38 +0100

    add getHook test

--- test/repo_spec.js
+++ test/repo_spec.js
@@ -1,9 +1,10 @@
-/* global describe it after */
+/* global describe it after beforeEach afterEach */
 var assert = require('assert')
 var repo = require('../lib/repo')
 var fs = require('fs')
 var fse = require('fs-extra')
 var path = require('path')
+var sinon = require('sinon')
 
 var gitDir = path.join(__dirname, 'here')
 
@@ -34,6 +35,31 @@ describe('The repo module', function () {
   it('checkout("http://github.com/lipp/doclets", "foobla", "./here") throws', function () {
     assert.throws(function () {
       repo.checkout('http://github.com/lipp/doclets', 'foobla', gitDir)
+    })
+  })
+
+  describe('working with the node_github package', function () {
+    var sandbox
+
+    beforeEach(function () {
+      sandbox = sinon.sandbox.create()
+    })
+
+    afterEach(function () {
+      sandbox.restore()
+    })
+
+    it('.getHook()', function (done) {
+      sinon.stub(repo.github(), 'authenticate').returns()
+      sinon.stub(repo.github().repos, 'getHooks')
+        .withArgs({user: 'lipp', repo: 'bar', per_page: 100})
+        .yields(null, [{config: {url: 'http://api.doclets.io/github/callback'}}])
+
+      repo.getHook('lipp', 'bar', {foo: 1}, function (err, hook) {
+        assert(!err)
+        assert.equal(hook.config.url, 'http://api.doclets.io/github/callback')
+        done()
+      })
     })
   })
 })
