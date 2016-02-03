1bbd3b20d14024c8af0e45f01b42d33aee86d63a
Gerhard Preuss <gelipp@gmail.com>
2016-02-03 16:56:03 +0100

    add Docket.findByOwner

--- test/routes_spec.js
+++ test/routes_spec.js
@@ -2,10 +2,11 @@
 var assert = require('assert')
 var Routes = require('../lib/routes').Routes
 var sinon = require('sinon')
-// var Repo = require('../lib/models/repo')
-// var Doclet = require('../lib/models/doclet')
+var Repo = require('../lib/models/repo')
+var Doclet = require('../lib/models/doclet')
 var User = require('../lib/models/user')
-// var _ = require('underscore')
+var _ = require('underscore')
+var moment = require('moment')
 
 describe('The routes module', function () {
   var routes
@@ -76,7 +77,7 @@ describe('The routes module', function () {
     assert(next.called)
   })
 
-  it('.ensureAuthenticated(req, res, next) calls res.redirect("/login") if not authenticated', function () {
+  it('.ensureAuthenticated(req, res, next) calls res.redirect("/") if not authenticated', function () {
     var next = sinon.spy()
     var req = {
       isAuthenticated: function () {
@@ -88,84 +89,68 @@ describe('The routes module', function () {
     }
     routes.ensureAuthenticated(req, res, next)
     assert.equal(next.called, false)
-    assert(res.redirect.calledWith('/login'))
+    assert(res.redirect.calledWith('/'))
   })
 
-  /*
-  it('.account(req, res) calls Repo.findByUser and res.render("account.jade")', function () {
-    sandbox.stub(Repo, 'findByUser').yields(null, 123)
+  it('.user(req, res) (own repo) res.render("user.jade")', function () {
+    sandbox.stub(Doclet, 'findByOwner').yields(null, 345)
+    sandbox.stub(User, 'findById').yields(null, 555)
+    sandbox.stub(Repo, 'findOrSyncByUser').yields(null, 123)
     var req = {
       user: {
         _id: 'asd',
         token: 'asd'
+      },
+      params: {
+        user: 'asd'
       },
       query: {}
     }
     var res = {}
     res.render = sinon.spy()
-    routes.account(req, res)
+    routes.user(req, res)
     var resArgs = res.render.args[0]
-    assert.equal(resArgs[0], 'account.jade')
-    assert.deepEqual(resArgs[1], {user: req.user, repos: 123, _: _, owner: req.user})
+    assert.equal(resArgs[0], 'user.jade')
+    assert.deepEqual(resArgs[1], {
+      user: req.user,
+      repos: 123,
+      _: _,
+      owner: 555,
+      doclets: 345,
+      moment: moment,
+      username: 'asd'
+    })
   })
 
-  it('.getAccountRepo(req, res) calls Repo.findById and Doclet.find and res.render("repo.jade")', function () {
-    sandbox.stub(Repo, 'findById').yields(null, 123)
-    sandbox.stub(Doclet, 'find').yields(null, 333)
+  it('.user(req, res) (other repo) res.render("user.jade")', function () {
+    sandbox.stub(Doclet, 'findByOwner').yields(null, 345)
+    sandbox.stub(User, 'findById').yields(null, 555)
+    sandbox.stub(Repo, 'find').yields(null, 123)
     var req = {
       user: {
-        _id: 'asd',
+        _id: 'asd2',
         token: 'asd'
       },
       params: {
-        repo: 'tt'
+        user: 'asd'
       },
-      flash: sinon.spy(function (which) {
-        return which === 'error' ? [1] : [2]
-      })
+      query: {}
     }
     var res = {}
     res.render = sinon.spy()
-    routes.getAccountRepo(req, res)
+    routes.user(req, res)
     var resArgs = res.render.args[0]
-    assert.equal(resArgs[0], 'repo.jade')
-    var expectedParams = {
+    assert.equal(resArgs[0], 'user.jade')
+    assert.deepEqual(resArgs[1], {
       user: req.user,
-      repo: 123,
+      repos: 123,
       _: _,
-      doclets: 333,
-      owner: req.user,
-      error: 1,
-      result: 2
-    }
-    assert.deepEqual(resArgs[1], expectedParams)
-    assert(req.flash.calledWith('error'))
-    assert(req.flash.calledWith('result'))
+      owner: 555,
+      doclets: 345,
+      moment: moment,
+      username: 'asd'
+    })
   })
-
-  it('.setAccountRepo(req, res) calls Repo.findById and enables webhook and res.redirect("/account/<repo>")', function () {
-    var repo = {}
-    repo.enableWebHook = sinon.stub().yields(null)
-    sandbox.stub(Repo, 'findById').yields(null, repo)
-    var req = {
-      user: {
-        _id: 'asd',
-        token: 'asd'
-      },
-      params: {
-        repo: 'tt'
-      },
-      body: {
-        enabled: 'on'
-      },
-      flash: sinon.spy()
-    }
-    var res = {}
-    res.redirect = sinon.spy()
-    routes.setAccountRepo(req, res)
-    assert(res.redirect.calledWith('/account/tt'))
-    assert(repo.enableWebHook.calledWith())
-  }) */
 
   it('.serializeUser() creating a new User', function (done) {
     var ghUser = {
