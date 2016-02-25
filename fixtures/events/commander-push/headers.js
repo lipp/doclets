52039173edef707d97f6163a67ad2297454e27a6
Gerhard Preuss <gelipp@gmail.com>
2016-02-25 10:38:26 +0100

    add commander fixture

---
+++ fixtures/events/commander-push/headers.js
@@ -0,0 +1,8 @@
+module.exports = {
+		method: 'POST',
+		'content-type': 'application/json',
+		'User-Agent': 'GitHub-Hookshot/35ff2c9',
+		'X-GitHub-Delivery': '3ef75d00-b647-11e5-8d27-48af50c16bcd',
+		'X-GitHub-Event': 'push',
+		'X-Hub-Signature': 'sha1=9bb29bb418c57488301e5084639c6bdf3400f182' // reverse engineered with hacked github-webhook-handler
+	}
