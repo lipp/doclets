4551939a7707ff66ee8ec8be3d0bf2d234c79012
Gerhard Preuss <gelipp@gmail.com>
2016-04-08 17:50:29 +0200

    fix closure param links

---
+++ fixtures/closure-syntax/index.js
@@ -0,0 +1,12 @@
+
+/**
+ * Makes some foo
+ * @param {Array<Point>} points An array of points on which to foo
+ * @param {Object} [options]
+ * @param {string} options.mode The mode of fooing
+ * @param {module:foo/bar~Bla.bla} [bla] The real bla
+ * @param {Object<string, number>|Array<number>} [magics] Magic numbers
+ * @return {Promise< Array<Point >>} The resulting food points as promise
+ */
+var foo = function(points) {
+}
